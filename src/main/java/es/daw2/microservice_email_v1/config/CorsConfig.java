415f766b47e5e6e9e73c66518dfd2fd2e20aaf15
andrew-gb-dev <andersalcedoolivera1@gmail.com>
2025-06-11 21:38:30 +0200

    feat(cors): añadir configuración CORS para permitir peticiones desde el cliente     - Se ha añadido la clase CorsConfig para habilitar CORS correctamente     - Se ha añadido Dockerfile para dockerizar el microservicio de email

---
+++ src/main/java/es/daw2/microservice_email_v1/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package es.daw2.microservice_email_v1.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
